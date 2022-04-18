b9c69a0357477788ed8b7f1d069d1ebed9140caf
Felipe Matheus <fmatheusog@gmail.com>
2022-04-18 15:54:08 -0300

    creates postscategories migration

---
+++ migrations/20220418005903-create-postscategories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        allowNull: false,
+        references: {
+          model: 'BlogPost',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
