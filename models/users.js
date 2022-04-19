885a74a3ad97646fec26cbb553f4e5f4ac1cc557
Felipe Matheus <fmatheusog@gmail.com>
2022-04-19 15:46:53 -0300

    creates users model

---
+++ models/users.js
@@ -0,0 +1,10 @@
+module.exports = (Sequelize, DataTypes) => {
+  const User = Sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return User;
+};
