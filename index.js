2fb0616437e37ff610ee863e7bf7d6ee11ebe0e0
Felipe Matheus <fmatheusog@gmail.com>
2022-04-19 23:32:21 -0300

    creates users route file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const UserRoutes = require('./routes/users');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/users', UserRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
